51c9076104f54caf6106bd1f9fe864c82eb68e93
HEREYUA <1240335630@qq.com>
2021-09-27 10:25:05 +0800

    修改header

--- src/components/BreadCrumb/BreadCrumb.jsx
+++ src/components/BreadCrumb/BreadCrumb.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import {Breadcrumb} from 'antd'
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
 
 const BreadCrumb = (props)=>(
   <Breadcrumb style={{marginBottom:16}}>
     {props.arr && props.arr.map(item=>{
-      if ((typeof item) === 'object'){
-        return <Breadcrumb.Item key={item.title}><Link to={item.to}>{item.title}</Link></Breadcrumb.Item>
-      } else {
         return <Breadcrumb.Item key={item}>{item}</Breadcrumb.Item>
-      }
+      
     })}
   </Breadcrumb>
 )
--- src/components/HeaderBar/HeaderBar.jsx
+++ src/components/HeaderBar/HeaderBar.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-// import {Button} from  'antd'
+import {Avatar,Menu,Dropdown} from  'antd'
 import{
     MenuUnfoldOutlined,
     MenuFoldOutlined
@@ -7,11 +7,33 @@ import{
 
 import './header.css'
 export default class HeaderBar extends Component {
+    state={
+        imgUrl:'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png'
+    }
     render() {
+        const {imgUrl}=this.state
+        const menu = (
+            <Menu className='menu'>
+              <Menu.ItemGroup title='用户中心' className='menu-group'>
+                <Menu.Item>你好</Menu.Item>
+                <Menu.Item>个人信息</Menu.Item>
+                <Menu.Item><span>退出登录</span></Menu.Item>
+              </Menu.ItemGroup>
+              <Menu.ItemGroup title='设置中心' className='menu-group'>
+                <Menu.Item>个人设置</Menu.Item>
+                <Menu.Item>系统设置</Menu.Item>
+              </Menu.ItemGroup>
+            </Menu>
+          )
         return (
-            <div>
-               <div type="primary" onClick={this.props.toggle} className="collapse-btn">
-                {this.props.collapsed ? <MenuUnfoldOutlined style={{fontSize:'20px'}}/>  : <MenuFoldOutlined style={{fontSize:'20px'}}/>}
+            <div className="header">
+               <div onClick={this.props.toggle} className="collapse-btn">
+                    {this.props.collapsed ? <MenuUnfoldOutlined style={{fontSize:'20px'}}/>  : <MenuFoldOutlined style={{fontSize:'20px'}}/>}
+                </div>
+                <div className="admin-info">
+                    <Dropdown overlay={menu}>
+                        <Avatar src={imgUrl} onClick={e=>e.preventDefault()}/>
+                    </Dropdown>
                 </div>
             </div>
         )
